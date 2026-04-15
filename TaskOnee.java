02b433015eca224033a6e7c8999d54e0631020ab
SamuelBossua <resmax010@gmail.com>
2026-04-15 17:08:23 +0100

    Add files via upload

---
+++ TaskOnee.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+public class TaskOnee {
+       public static void main(String[] args){
+       
+       Scanner input = new Scanner(System.in);
+       System.out.println("Enter 10 numbers:");
+       
+      int counter = 1;
+      
+
+      while(counter <= 10){
+      
+      int number = input.nextInt;
+      System.out.println(number);
+      counter++;
+
+
+
+
+
+
+
+
+
+      }
+
+
+int sum = sum + number;
+System.out.println(sum);
+
+
+
+
+
+
+
+
+
+
+}
+}
