27887ddf2c7b1ed1811beeef0a6c53b92c805fb8
SamuelBossua <resmax010@gmail.com>
2026-05-11 12:14:55 +0100

    Add files via upload

---
+++ ArrayTasks/DuplicateElimination.java
@@ -0,0 +1,11 @@
+public class DuplicateElimination {
+	public static void main(String[] args){
+
+	int[] values new int[10];
+	int[] values = {10,11,12,13,14,15,16,17,18,100};
+	int[] values = {-1} == [];
+		
+
+
+}
+}
